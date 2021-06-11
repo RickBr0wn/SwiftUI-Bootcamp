337593a713e9e4a11f322d4c3e91738bdd3b85a9
Rick Brown <ricky.brown.00@gmail.com>
2021-06-11 13:15:48 +0100

    Initial Commit

---
+++ SwiftUI-Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Rick Brown on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
