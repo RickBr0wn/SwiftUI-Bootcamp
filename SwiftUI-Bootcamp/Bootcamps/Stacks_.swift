c2ad89d627c8776395dc912de4d38697b81b6bce
Rick Brown <ricky.brown.00@gmail.com>
2021-06-24 21:25:45 +0100

    adds Stacks

---
+++ SwiftUI-Bootcamp/Bootcamps/Stacks_.swift
@@ -0,0 +1,20 @@
+//
+//  Stacks_.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Rick Brown on 24/06/2021.
+//
+
+import SwiftUI
+
+struct Stacks_: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct Stacks__Previews: PreviewProvider {
+  static var previews: some View {
+    Stacks_()
+  }
+}
