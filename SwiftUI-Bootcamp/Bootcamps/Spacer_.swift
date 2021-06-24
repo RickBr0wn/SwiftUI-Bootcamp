289d60f78872679a388676001d42bbd46ca6353c
Rick Brown <ricky.brown.00@gmail.com>
2021-06-24 21:27:17 +0100

    adds Spacer()

---
+++ SwiftUI-Bootcamp/Bootcamps/Spacer_.swift
@@ -0,0 +1,20 @@
+//
+//  Spacer_.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Rick Brown on 24/06/2021.
+//
+
+import SwiftUI
+
+struct Spacer_: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct Spacer__Previews: PreviewProvider {
+  static var previews: some View {
+    Spacer_()
+  }
+}
