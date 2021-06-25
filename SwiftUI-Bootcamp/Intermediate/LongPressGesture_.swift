c745cd90fa1ee2e7185eb3ede4ea44fc94f0392a
Rick Brown <ricky.brown.00@gmail.com>
2021-06-25 11:42:09 +0100

    adds Intermediate section

---
+++ SwiftUI-Bootcamp/Intermediate/LongPressGesture_.swift
@@ -0,0 +1,20 @@
+//
+//  LongPressGesture_.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Rick Brown on 24/06/2021.
+//
+
+import SwiftUI
+
+struct LongPressGesture_: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct LongPressGesture__Previews: PreviewProvider {
+  static var previews: some View {
+    LongPressGesture_()
+  }
+}
