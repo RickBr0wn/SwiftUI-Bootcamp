fb267671bcde914c85b45ff84d835ad702bf92ce
Rick Brown <ricky.brown.00@gmail.com>
2021-09-06 20:41:56 +0100

    adds custom button styles

--- SwiftUI-Bootcamp/Advanced/ButtonStyle_.swift
+++ SwiftUI-Bootcamp/Advanced/ButtonStyle_.swift
@@ -7,9 +7,38 @@
 
 import SwiftUI
 
+struct PressableButtonStyle: ButtonStyle {
+  let scale: Double
+  
+  init(scale: Double) {
+    self.scale = scale
+  }
+  
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .opacity(configuration.isPressed ? scale : 1.0)
+      // .brightness(configuration.isPressed ? 0.3 : 0.0)
+      .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
+  }
+}
+
+extension View {
+  
+  func withPressableButtonStyle(scaledAmount: Double = 0.9) -> some View {
+    self.buttonStyle(PressableButtonStyle(scale: scaledAmount))
+  }
+}
+
 struct ButtonStyle_: View {
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    Button(action: {}, label: {
+      Text("Click Me!!")
+        .font(.headline)
+        .withDefaultButtonFormatting()
+    })
+    // .withPressableButtonStyle()
+    .withPressableButtonStyle(scaledAmount: 0.9)
+    .padding(40)
   }
 }
 
